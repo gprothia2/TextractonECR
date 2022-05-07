381926603b8606cc22b44d409d20ea0abd0d4212
gprothia2 <81781486+gprothia2@users.noreply.github.com>
2022-05-07 10:11:57 -0700

    Update constants.py

--- main/EulerEye-master3/src/constants.py
+++ main/EulerEye-master3/src/constants.py
@@ -1,4 +1,4 @@
-S3_BUCKET_NAME = 'ancestory-gap2'
-S3_RAW_IMAGE_FOLDER = "ancestory-gap2/input/"
+S3_BUCKET_NAME = 'xxx'
+S3_RAW_IMAGE_FOLDER = "xxx/input/"
 TEXTRACT_LIMIT_SIZE = 10485760
 FCN_MODEL = 'last_20.pth.tar'
